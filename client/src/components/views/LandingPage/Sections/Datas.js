06cefc7371ab5eb2302edf2ecc3999e4a722d726
LvnReys16 <zethkennethpungtod@gmail.com>
2020-06-15 17:18:01 +0800

    first commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,53 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "₱0 to ₱1000",
+        "array": [0, 1000]
+    },
+    {
+        "_id": 2,
+        "name": "₱1000 to ₱10000",
+        "array": [1001, 5000]
+    },
+    {
+        "_id": 3,
+        "name": "₱10000 to ₱15000",
+        "array": [10000, 15000]
+    },
+    {
+        "_id": 4,
+        "name": "15000 to ₱30000",
+        "array": [15000, 30000]
+    },
+    {
+        "_id": 5,
+        "name": "More than ₱30000",
+        "array": [30000, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Local"
+    },
+    {
+        "_id": 2,
+        "name": "Overseas"
+    },
+   
+]
+
+
+export {
+    price,
+    continents
+}
